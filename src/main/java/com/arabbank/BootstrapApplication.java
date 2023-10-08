86d848be849692b58279512f8185055efbe93b0e
Abd Frehat <abood512543@gmail.com>
2023-10-08 11:57:20 +0300

    Update BootstrapApplication.java

--- src/main/java/com/arabbank/BootstrapApplication.java
+++ src/main/java/com/arabbank/BootstrapApplication.java
@@ -1,1 +1,8 @@
 
+@SpringBootApplication(scanBasePackages = {})
+  public class BootstrapApplication {
+
+    public static void main(String[] args) {
+      SpringApplication.run(SpringApplication.run(BootstrapApplication.class));
+    }
+  }
