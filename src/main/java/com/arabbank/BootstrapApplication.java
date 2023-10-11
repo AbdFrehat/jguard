3b9ce9a1b50190b9bf3d274e62b477c5aea30a16
Abd Frehat <abood512543@gmail.com>
2023-10-11 13:51:43 +0300

    Delete src/main/java/com/arabbank/BootstrapApplication.java

--- src/main/java/com/arabbank/BootstrapApplication.java
+++
@@ -1,8 +0,0 @@
-
-@SpringBootApplication(scanBasePackages = {})
-  public class BootstrapApplication {
-
-    public static void main(String[] args) {
-      SpringApplication.run(SpringApplication.run(BootstrapApplication.class));
-    }
-  }
